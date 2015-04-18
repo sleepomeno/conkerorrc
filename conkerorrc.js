ee664289ec9e9bc459b463d7d3b7b99368fdb2a4
Gregor Riegler <gregor.riegler@gmail.com>
2015-04-18 15:06:13 +0200

    Reopen closed tab

--- conkerorrc.js
+++ conkerorrc.js
@@ -65,6 +65,21 @@ download_buffer_automatic_open_target=OPEN_NEW_BUFFER_BACKGROUND;
 
 // cwd = get_home_directory();
 // cwd.append("/Downloads/");
+{
+   let _save_path = get_home_directory();
+
+   function update_save_path (info) {
+       _save_path = info.target_file.parent.path;
+   }
+
+   add_hook("download_added_hook", update_save_path);
+
+   suggest_save_path_from_file_name = function (filename, buffer) {
+       let file = make_file(_save_path);
+       file.append(filename);
+       return file.path;
+   }
+}
 
 content_handlers.set("application/pdf", content_handler_open_default_viewer);
 external_content_handlers.set("application/pdf", "evince");
@@ -125,13 +140,43 @@ clicks_in_new_buffer_target = OPEN_NEW_BUFFER_BACKGROUND;
 function define_switch_buffer_key (key, buf_num) { define_key(default_global_keymap, key, function (I) { switch_to_buffer(I.window, I.window.buffers.get_buffer(buf_num)); }); } for (let i = 0; i < 10; ++i) { define_switch_buffer_key(String((i+1)%10), i); }
 tab_bar_show_icon = true;
 
+var my_closed_buffers = new Array();
+//save the URL of the current buffer before closing it
+interactive("my-close-and-save-current-buffer",
+  "close and save the current buffer for later restore",
+  function(I) {
+      if(my_closed_buffers.length==10){
+        my_closed_buffers.shift(); // remove older item to save
+        // memory, just save maximum 10 buffers
+    }
+    my_closed_buffers.push(I.buffer.document.URL);
+    kill_buffer(I.buffer); //kill the current buffer
+  });
+undefine_key(default_global_keymap, "q");
+define_key(default_global_keymap, "q", "my-close-and-save-current-buffer");
+interactive("my-open-closed-buffer",
+  "open the last closed buffer", 
+  function(I){
+    // check if the array length > 0
+    if(my_closed_buffers.length>0){
+      // load the URL in new windows
+      load_url_in_new_buffer(
+        my_closed_buffers[my_closed_buffers.length - 1], I.window);
+      // remove the first item in the array
+      my_closed_buffers.pop();
+    }
+  });
+undefine_key(default_global_keymap, "X");
+define_key(default_global_keymap, "X", "my-open-closed-buffer")
+define_key(default_global_keymap, "A", "my-open-closed-buffer")
+
 define_key(content_buffer_normal_keymap, "F", "follow-new-buffer-background"); 
 define_key(content_buffer_normal_keymap, "G", "find-url-new-buffer");
 
-define_key(default_global_keymap, "x", "kill-current-buffer");
-define_key(content_buffer_normal_keymap, "x", "kill-current-buffer");
-define_key(content_buffer_normal_keymap, "C-w", "kill-current-buffer");
-define_key(default_global_keymap, "C-w", "kill-current-buffer");
+define_key(default_global_keymap, "x", "my-close-and-save-current-buffer");
+define_key(content_buffer_normal_keymap, "x", "my-close-and-save-current-buffer");
+define_key(content_buffer_normal_keymap, "C-w", "my-close-and-save-current-buffer");
+define_key(default_global_keymap, "C-w", "my-close-and-save-current-buffer");
 
 // Reload buffer
 define_key(content_buffer_normal_keymap, "f5", "reload");
